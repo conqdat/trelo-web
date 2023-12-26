228dc692909d63f90bc8e5cacc41aa2a4619c27c
DatTran <trancongdat.datct@gmail.com>
2023-12-26 21:38:52 +0700

    setting dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,30 @@ import DeleteIcon from '@mui/icons-material/Delete'
 import AndroidIcon from '@mui/icons-material/Android'
 
 import Button from '@mui/material/Button'
+import {
+  Experimental_CssVarsProvider as CssVarsProvider,
+  experimental_extendTheme as extendTheme,
+  useColorScheme
+} from '@mui/material/styles'
+
+function ModeToggle() {
+  const { mode, setMode } = useColorScheme()
+  return (
+    <Button
+      onClick={() => {
+        setMode(mode === 'light' ? 'dark' : 'light')
+      }}
+    >
+      {mode === 'light' ? 'Turn dark' : 'Turn light'}
+    </Button>
+  )
+}
 
 function App() {
   return (
     <>
+      <ModeToggle />
+      <hr />
       <h1>React App</h1>
       <Button variant="contained">Hello world</Button>
       <Button variant="text">Text</Button>
