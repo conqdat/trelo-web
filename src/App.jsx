15d7eecfc21858273cc286add94e6900f0d08610
DatTran <trancongdat.datct@gmail.com>
2023-12-27 13:41:00 +0700

    add icon mui

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,53 @@ import {
   useColorScheme
 } from '@mui/material/styles'
 
+import FormControl from '@mui/material/FormControl'
+import InputLabel from '@mui/material/InputLabel'
+import * as React from 'react'
+import useMediaQuery from '@mui/material/useMediaQuery'
+import MenuItem from '@mui/material/MenuItem'
+import Select from '@mui/material/Select'
+import LightModeIcon from '@mui/icons-material/LightMode'
+import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined'
+import SettingsBrightnessOutlinedIcon from '@mui/icons-material/SettingsBrightness'
+
+function ModeSelect() {
+  const { mode, setMode } = useColorScheme()
+
+  const handleChange = (event) => {
+    setMode(event.target.value)
+  }
+
+  return (
+    <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+      <InputLabel id="label-dark-light-mode">Mode</InputLabel>
+      <Select
+        labelId="label-dark-light-mode"
+        id="label-dark-light-mode"
+        value={mode}
+        label="Mode"
+        onChange={handleChange}
+      >
+        <MenuItem value="light">
+          <div style={{ display: 'flex', alignItems: 'center', gap: '8px' }}>
+            <LightModeIcon fontSize="small" /> Light
+          </div>
+        </MenuItem>
+        <MenuItem value="dark">
+          <div style={{ display: 'flex', alignItems: 'center', gap: '8px' }}>
+            <DarkModeOutlinedIcon fontSize="small" /> Dark
+          </div>
+        </MenuItem>
+        <MenuItem value="system">
+          <div style={{ display: 'flex', alignItems: 'center', gap: '8px' }}>
+            <SettingsBrightnessOutlinedIcon fontSize="small" /> System
+          </div>
+        </MenuItem>
+      </Select>
+    </FormControl>
+  )
+}
+
 function ModeToggle() {
   const { mode, setMode } = useColorScheme()
   return (
@@ -24,6 +71,7 @@ function ModeToggle() {
 function App() {
   return (
     <>
+      <ModeSelect />
       <ModeToggle />
       <hr />
       <h1>React App</h1>
